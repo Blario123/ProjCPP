f1c4122c47a8c396298b5b9aafa487a8484f57b1
Blario123 <bliversidge123@gmail.com>
2020-10-10 13:02:28 +0100

    ---

--- main.cpp
+++ main.cpp
@@ -11,58 +11,51 @@
 #define ASIO_STANDALONE
 #define UTC (0)
 
-#include <time.h>
 #include <asio.hpp>
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
-#include <boost/bind/bind.hpp>
+#include <time.h>
 
-int main()
-{
-    bool connected = false;
-    while (!connected){
-        asio::error_code error;
-        asio::io_context context;
-        asio::socket_base::keep_alive ka(true);
+int main() {
+  bool connected = false;
+  while (!connected) {
+    asio::error_code error;
+    asio::io_context context;
+    asio::socket_base::keep_alive ka(true);
 
-        asio::ip::tcp::endpoint endpoint(asio::ip::make_address("192.168.0.100", error), 8081);
-        asio::ip::tcp::socket socket(context);
+    asio::ip::tcp::endpoint endpoint(
+        asio::ip::make_address("192.168.0.100", error), 8081);
+    asio::ip::tcp::socket socket(context);
 
-        socket.connect(endpoint, error);
-        socket.set_option(ka);
-        time_t ertime = time(NULL);
-        if(!error){
-            std::cout << "Connected to " << endpoint.address() << std::endl;
+    socket.connect(endpoint, error);
+    socket.set_option(ka);
+    time_t ertime = time(NULL);
+    if (!error) {
+      std::cout << "Connected to " << endpoint.address() << std::endl;
 
-            std::string request =
-                    "GET / HTTP1.1\r\n"
-                    "Host: 192.168.0.100\r\n"
-                    "Connection: close\r\n\r\n";
+      std::string request = "GET / HTTP1.1\r\n"
+                            "Host: 192.168.0.100\r\n"
+                            "Connection: close\r\n\r\n";
 
-            socket.write_some(asio::buffer(request.data(), request.size()), error);
+      socket.write_some(asio::buffer(request.data(), request.size()), error);
 
-            size_t bytes = socket.available();
-            if(bytes > 0){
-                std::vector<char> vBuffer(bytes);
-                socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), error);
+      size_t bytes = socket.available();
+      if (bytes > 0) {
+        std::vector<char> vBuffer(bytes);
+        socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), error);
 
-                for (auto c : vBuffer){
-                    std::cout << c;
-                }
-            }
-            else {
-                std::cout << "No bytes to be read" << std::endl;
-            }
-            connected = true;
+        for (auto c : vBuffer) {
+          std::cout << c;
         }
-        else{
-            std::cout << "Failed to connect\n"
-                      << "ERROR:\t"
-                      << strtok(ctime(&ertime), "\n")
-                      << "\t"
-                      << error.message()
-                      << std::endl;
-        }
+      } else {
+        std::cout << "No bytes to be read" << std::endl;
+      }
+      connected = true;
+    } else {
+      std::cout << "Failed to connect\n"
+                << "ERROR:\t" << strtok(ctime(&ertime), "\n") << "\t"
+                << error.message() << std::endl;
     }
-    return 0;
+  }
+  return 0;
 }
